8786352f2a5b04bee384dda7c50796c53e9b22f5
Kuba Kazmierczak <jakub.kazmierczak@netguru.com>
2020-12-26 10:51:04 +0100

    5 completed

--- 5/two.rb
+++ 5/two.rb
@@ -6,7 +6,7 @@ module Solution
   end
 
   def row(string, arr)
-    puts "string: #{string}, arr: #{arr}"
+    # puts "string: #{string}, arr: #{arr}"
 
     return arr[0] if arr[0] == arr[1]
 
@@ -17,13 +17,12 @@ module Solution
   end
 
   def column(string, arr)
-    puts "string: #{string}, arr: #{arr}"
-    puts "arr[0]: #{arr[0]}"
+    # puts "string: #{string}, arr: #{arr}"
+    # puts "arr[0]: #{arr[0]}"
     return arr[0] if arr[0] == arr[1]
 
     front_arr = [arr[0], arr[1] - (arr[1] - arr[0]) / 2 - 1]
     back_arr = [arr[1] - (arr[1] - arr[0]) / 2, arr[1]]
-    puts "string[0] == 'L': #{string[0] == 'L'}"
 
     string[0] == 'L' ? column(string[1..-1], front_arr) : column(string[1..-1], back_arr)
   end
@@ -31,13 +30,17 @@ module Solution
   def value(code)
     row = row(code[0..6], [0, 127])
     column = column(code[7..9], [0, 7])
-    puts "row: #{row}"
-    puts "column: #{column}"
+    # puts "row: #{row}"
+    # puts "column: #{column}"
     row * 8 + column
   end
 
   def call
-    File.readlines('./5/input.txt').map { |code| { code => value(code) } }.sort_by { |_key, value| value }
+    ids = File.readlines('./5/input.txt').map { |code| value(code) }.sort
+    # puts ids
+    puts (ids[0]..ids[-1]).to_a
+
+    (ids[0]..ids[-1]).to_a - ids
   end
 end
 
--- start.rb
+++ start.rb
@@ -3,3 +3,8 @@
 def start(file)
   load file
 end
+
+def start_module(folder, file)
+  load "./#{folder}/#{file}.rb"
+  include Solution
+end
