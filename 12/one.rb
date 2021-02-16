4966d9ef8728f929d77d3e661476a58e75f226df
Kuba Kazmierczak <jakub.kazmierczak@netguru.com>
2021-02-16 21:23:17 +0100

    Finish exercise one

--- 12/one.rb
+++ 12/one.rb
@@ -5,6 +5,28 @@ module Solution
   include DataConverter
   def result(file)
     insts = lines(file).map { |line| [line[0], line[1..-1].to_i] }
+    count(steps('E', insts))
+  end
+
+  def count(hash)
+    (hash['E'] - hash['W']).abs + (hash['N'] - hash['S']).abs
+  end
+
+  def steps(initial, instructions)
+    result = { 'N' => 0, 'E' => 0, 'S' => 0, 'W' => 0 }
+    direction = initial
+    instructions.each do |i|
+      puts "instruction: #{i}, result: #{result}"
+      case i[0]
+      when 'R', 'L'
+        direction = change_dir(direction, i[0], i[1])
+      when 'F'
+        result[direction] += i[1]
+      else
+        result[i[0]] += i[1]
+      end
+    end
+    result
   end
 
   def change_dir(initial, direction, val)
